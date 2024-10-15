ce03b590027f582ae873f1be5dfca8ab9909d468
Kyle Yang <kyleyang2020@gmail.com>
2024-10-15 12:10:51 -0400

    work

--- GP3/Assets/Scripts/Spooky/TrickOrTreat.cs
+++ GP3/Assets/Scripts/Spooky/TrickOrTreat.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TrickOrTreat : MonoBehaviour
 {
+    public Animator anim;
+    public GameObject Canvas;
+    public GameObject Anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,5 +24,11 @@ public class TrickOrTreat : MonoBehaviour
     public void Check()
     {
         Debug.Log("button");
+        Canvas.SetActive(false);
+        Anim.SetActive(true);
+        if (anim != null)
+        {
+            anim.SetTrigger("Roll");
+        }
     }
 }
